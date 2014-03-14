9f2d52afbd82a02f236121d00105b065113fafb0
Jeph Paul <jephpaul@gmail.com>
2014-03-15 02:07:30 +0530

    Adding the link to the evenst page from the banner & minor js fixes

--- editCount/static/js/show_detail.js
+++ editCount/static/js/show_detail.js
@@ -1,30 +1,16 @@
 $(function(){
-	var page = 206337;
 	var ajax_call = function(){
 		$.ajax({
-			url: 'http://ec2-54-186-46-239.us-west-2.compute.amazonaws.com/editCount/api/v1.0/article/',
+			url: '/editCount/api/v1.0/article/',
 			type: 'GET',
 			dataType: 'json',
 		})
 		.done(function( data ) {
-			//console.log( data["Usha_Meena"] );
-			//console.log(data.object);
 			var values;
 			$.each( data, function( key, val ) {
 				values = val;
 			});
 			var count = 1, articleData, totalEdit = 0;
-  			// $.each(values, function(key, val) {
-  			// 	 /* iterate through array or object */
-  			// 	 console.log(key + val);
-  			// 	 if (key === 'timestamp')
-  			// 	 	continue;
-  			// 	 key = key.replace('_', ' ')
-  			// 	 articleData = "<tr><td> " + count + " </td> <td>" + key+ "</td> <td>" + val+ "</td> </tr>";
-  			// 	 $('#articleData').append(articleData);
-  			// 	 count = count + 1;
-  				// });
-  			// https://en.wikipedia.org/wiki/key
   			
   			$('#articleData').find('tr').remove();
 
@@ -39,7 +25,7 @@ $(function(){
 				$('#articleData').append(articleData);
 				count = count + 1;
 			}
-			$('#totalArticleCount').text(count);
+			$('#totalArticleCount').text(count-1);
 			$('#totalEditCount').text(totalEdit);
 
 		})
@@ -49,7 +35,7 @@ $(function(){
 		.always(function() {
 			console.log("complete");
 		});
-	}
+	};
 	ajax_call();
 	setInterval(ajax_call, 1000*60*3);
 });
