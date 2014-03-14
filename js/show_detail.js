a02969c892b2a06f793d2ad91d424a8eed678c2f
harshkothari410 <harshkothari410@gmail.com>
2014-03-14 17:33:17 +0530

    create tables using APIs

---
+++ js/show_detail.js
@@ -0,0 +1,47 @@
+$(function(){
+	var page = 206337;
+	$.ajax({
+		url: 'http://127.0.0.1:5000/editCount/api/v1.0/article/',
+		type: 'GET',
+		dataType: 'json',
+	})
+	.done(function( data ) {
+		//console.log( data["Usha_Meena"] );
+		//console.log(data.object);
+		var values;
+		$.each( data, function( key, val ) {
+			values = val;
+		});
+		var count = 1, articleData, totalEdit = 0;
+  		// $.each(values, function(key, val) {
+  		// 	 /* iterate through array or object */
+  		// 	 console.log(key + val);
+  		// 	 if (key === 'timestamp')
+  		// 	 	continue;
+  		// 	 key = key.replace('_', ' ')
+  		// 	 articleData = "<tr><td> " + count + " </td> <td>" + key+ "</td> <td>" + val+ "</td> </tr>";
+  		// 	 $('#articleData').append(articleData);
+  		// 	 count = count + 1;
+  			// });
+
+		for ( var key in values){
+			if (key === 'timestamp')
+				continue;
+			val = values[key];
+			totalEdit = totalEdit + val;
+			key = key.replace(/\_/g,' ');
+			articleData = "<tr><td> " + count + " </td> <td>" + key+ "</td> <td>" + val + "</td> </tr>";
+			$('#articleData').append(articleData);
+			count = count + 1;
+		}
+
+		$('#totalEdit').text('Total Edit : ' + totalEdit);
+
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+})
